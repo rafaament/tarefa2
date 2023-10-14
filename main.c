922da6c41cd9ac4ec6b41fbdd399cdfd859ed2bf
rafaament <rafael.1995@live.com>
2023-10-14 15:05:42 -0300

    iniciando projeto

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+   int contador = 0;
+  
+    for(contador = 0; contador <= 10; contador++)
+    {
+        printf("Hello world! %d \n", contador);
+    }
+    return 0;
+}
